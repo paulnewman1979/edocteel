f45b5abf12002eca38c4cc1708cf0c91dc859586
Tao Liu <taoliu@yahoo-inc.com>
2016-08-29 15:02:03 -0700

    104

---
+++ 104/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+		if (NULL == root) {
+			return 0;
+		}
+
+		int leftDepth = maxDepth(root->left);
+		int rightDepth = maxDepth(root->right);
+		return (leftDepth>rightDepth)?(leftDepth+1):(rightDepth+1);
+    }
+};
