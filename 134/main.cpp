edd8c725ccd15da7f8db56c9f32b7dff2453bafd
Tao Liu <taoliu@yahoo-inc.com>
2016-10-24 15:09:07 -0700

    134

--- 134/main.cpp
+++ 134/main.cpp
@@ -16,7 +16,11 @@ public:
         if (total < 0) {
             return -1;
         }
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> 0f2002edfeb8897b5ef3a66247fa9daf12604db0
         total = 0;
         int curStart = 0;
         for (i=0; i<gas.size(); ++i) {
@@ -26,13 +30,18 @@ public:
                 curStart = i + 1;
             }
         }
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> 0f2002edfeb8897b5ef3a66247fa9daf12604db0
         return curStart;
     }
 };
 
 int main(int argc, char* argv[])
 {
+<<<<<<< HEAD
 	int n;
 	vector<int> gas;
 	vector<int> cost;
@@ -51,6 +60,26 @@ int main(int argc, char* argv[])
 
     Solution solution;
 	cout << solution.canCompleteCircuit(gas, cost) << endl;
+=======
+    int n;
+    vector<int> gas;
+    vector<int> cost;
+
+    cin >> n;
+    while (n != -999999) {
+        gas.push_back(n);
+        cin >> n;
+    }
+
+    cin >> n;
+    while (n != -999999) {
+        cost.push_back(n);
+        cin >> n;
+    }
+
+    Solution solution;
+    cout << solution.canCompleteCircuit(gas, cost) << endl;
+>>>>>>> 0f2002edfeb8897b5ef3a66247fa9daf12604db0
 
     return 0;
 }
