c63a439fba9286a2dd99e53da077d2edc790bec6
TAO LIU <paulnewman1979lt@gmail.com>
2018-09-26 16:33:38 -0700

    897

---
+++ 89/897/main.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        if (NULL == root) return NULL;
+        TreeNode* head;
+        TreeNode* tail;
+        head = increasingBST(root, tail);
+        return head;
+    }
+
+private:
+    TreeNode* increasingBST(TreeNode* root, TreeNode*& tail) {
+        TreeNode* head = root;
+        tail = root;
+
+        if (root->left != NULL) {
+            TreeNode* tmpTail;
+            head = increasingBST(root->left, tmpTail);
+            tmpTail->right = root;
+            root->left = NULL;
+        }
+
+        if (root->right != NULL) {
+            TreeNode* tmpHead = tail;
+            tmpHead->right = increasingBST(root->right, tail);
+        }
+
+        return head;
+    }
+};
+
+TreeNode* composeTree() {
+    unsigned int n;
+    unsigned int i;
+    unsigned int index;
+    int value;
+    string lindex;
+    string rindex;
+    unsigned int leftIndex;
+    unsigned int rightIndex;
+
+    cin >> n;
+    vector<TreeNode*> nodes(n, NULL);
+    i = 0;
+    while (i < n) {
+        cin >> index;
+        cin >> value;
+        cin >> lindex;
+        cin >> rindex;
+        assert(i == index);
+
+        if (nodes[i] == NULL) {
+            nodes[i] = new TreeNode(value);
+        } else {
+            nodes[i]->val = value;
+        }
+        if (lindex != std::string("n")) {
+            leftIndex = atoi(lindex.c_str());
+            assert(leftIndex < n);
+            if (nodes[leftIndex] == NULL) {
+                nodes[leftIndex] = new TreeNode(0);
+            }
+            nodes[i]->left = nodes[leftIndex];
+        }
+        if (rindex != std::string("n")) {
+            rightIndex = atoi(rindex.c_str());
+            assert(rightIndex < n);
+            if (nodes[rightIndex] == NULL) {
+                nodes[rightIndex] = new TreeNode(0);
+            }
+            nodes[i]->right = nodes[rightIndex];
+        }
+        ++i;
+    }
+
+    return nodes[0];
+}
+
+void printNode(TreeNode* root, unsigned int rootIndex, unsigned int& index) {
+    unsigned int leftIndex = index;
+    cout << rootIndex << "\t" << root->val;
+    if (root->left) {
+        ++index;
+        cout << "\t" << leftIndex;
+    } else {
+        leftIndex = 0;
+    }
+    unsigned int rightIndex = index;
+    if (root->right) {
+        ++index;
+        cout << "\t" << rightIndex;
+    } else {
+        rightIndex = 0;
+    }
+    cout << endl;
+
+    if (leftIndex != 0) {
+        printNode(root->left, leftIndex, index);
+    }
+    if (rightIndex != 0) {
+        printNode(root->right, rightIndex, index);
+    }
+}
+
+void clearTree(TreeNode* root) {
+    if (NULL != root) {
+        clearTree(root->left);
+        clearTree(root->right);
+    }
+    delete root;
+}
+
+int main(int argc, char* argv[])
+{
+    TreeNode* root = composeTree();
+        
+    Solution solution;
+    TreeNode* newRoot = solution.increasingBST(root);
+    TreeNode* cur = newRoot;
+    while (cur != NULL) {
+        assert(cur->left == NULL);
+        cout << cur->val << endl;
+        cur = cur->right;
+    }
+
+    //unsigned index = 1;
+    //printNode(root, 0, index);
+
+    clearTree(root);
+
+    return 0;
+}
