8c377f17fe20b7f1c8cceae3dca7d8f2eaf7d162
TAO LIU <taoliu@verizonmedia.com>
2019-03-13 23:44:55 -0700

    template

---
+++ template/string_to_string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string toGoatLatin(string S) {
+        string result;
+        return result;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+    string str;
+	getline(std::cin, str);
+    Solution solution;
+	cout << solution.toGoatLatin(str) << endl;
+
+    return 0;
+}
