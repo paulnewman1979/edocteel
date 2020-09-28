15aee0b0368f30201476eab6c0e2577f3b4ad5ac
TAO LIU <taoliu@verizonmedia.com>
2020-09-28 13:20:47 -0700

    709

---
+++ 70/709/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include "../../template/common/Input.hh"
+#include "../../template/common/Output.hh"
+
+
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        string ret = str;
+        for (auto& ch: ret) {
+            if ((ch >= 'A') && (ch <= 'Z')) ch = ch - 'A' + 'a';
+        }
+        return ret;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+    // input
+    string str;
+    Input(str);
+    Output(str);
+
+    // solution
+    Solution solution;
+	string ret = solution.toLowerCase(str);
+
+    // output
+    Output(ret);
+
+    return 0;
+}
