8aeb2f45de5b9b2543dafc2093a29a7185409910
TAO LIU <taoliu@verizonmedia.com>
2021-02-01 20:52:30 -0800

    538

--- 53/538/main.cpp
+++ 53/538/main.cpp
@@ -14,7 +14,18 @@ typedef BaseTreeNode<int> TreeNode;
 class Solution {
 public:
     TreeNode* convertBST(TreeNode* root) {
+        if (NULL != root) {
+            convertBST(root, 0);
+        }
         return root;
+    }
+
+private:
+    int convertBST(TreeNode* root, int otherSum) {
+        if (root->right) root->val += convertBST(root->right, otherSum);
+        int sum = root->val + (root->left ? convertBST(root->left, root->val + otherSum) : 0);
+        root->val += otherSum;
+        return sum;
     }
 };
 
